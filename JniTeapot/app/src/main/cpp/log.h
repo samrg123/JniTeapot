35925b063b5ce3494698dd3d3a18f4ec58cba271
Samuel Gonzalez <samrg123@gmail.com>
2021-07-18 20:06:10 -0400

    Added Support for logging strings longer than max logcat payload size

--- JniTeapot/app/src/main/cpp/log.h
+++ JniTeapot/app/src/main/cpp/log.h
@@ -6,6 +6,7 @@
 	#include "macros.h"
 	#include "util.h"
 	#include "types.h"
+	#include "memUtil.h"
 
 	#include <android/log.h>
 	#include <stdio.h>
@@ -51,13 +52,119 @@
 		private:
             static constexpr bool kLogOutputDebug_ 	= true;
 
+			//Note: LOGGER_ENTRY_MAX_PAYLOAD is defined in liblog/include/log/log.h to be 4068
+			//		this limits the maximum number of bytes __android_log_print can log and will
+			//		truncate messages longer than it. Because liblog is built with the kernel we have
+			//		no way of telling what the actual maximum is? We spilt android log calls into 
+			//		multiples of kLogcatMaxPayloadBytes to prevent logs from being truncated.
+			//Note: We use kLogcatMaxPayloadBytes=4000 instead of 4068 to allow future revisions of android 
+			//		to add metadata to the log which shrinks the maximum payload bytes.
+			//TODO: Find out if there is actually a way to query what LOGGER_ENTRY_MAX_PAYLOAD is!	
+			//TODO: Make sure that LOGGER_ENTRY_MAX_PAYLOAD doesn't also include sizeof(kLogTag)-1
+			//TODO: Why doesn't logcat spit out 4k chars... only spits out 1024!?! 
+			static constexpr uint32 kLogcatMaxPayloadBytes = 1024;
+			static constexpr const char* kLogcatTag = " => JNI Native Logger";
+
 			template<typename ...ArgsT>
-			void LogStr(uint32 type, const ArgsT&... args) {
+			void LogStr(uint32 type, const char* fmt, const ArgsT&... args) {
 
 				//TODO: add more logging options
+				//TODO: look into __android_log_set_logger API 30
 
 				if constexpr(kLogOutputDebug_) {
-				    __android_log_print(AndroidLogPriority(type), " => JNI Native Logger", args...);
+
+					auto logPriority = AndroidLogPriority(type);
+					uint32 logBytes = snprintf(nullptr, 0, fmt, args...)+1;
+
+					if(logBytes <= kLogcatMaxPayloadBytes) __android_log_print(logPriority, kLogcatTag, fmt, args...);
+					else {
+
+						//attempt to allocate buffer
+						char* logBuffer = HeapAllocate(logBytes);
+
+						//Just truncate the log for now
+						//TODO: find way to print this using a fixed buffer
+						if(logBuffer == InvalidHeapPtr) {
+							__android_log_print(logPriority, kLogcatTag, fmt, args...);
+							return;
+						}
+
+						//print log string
+						snprintf(logBuffer, logBytes, fmt, args...);
+
+						//Split the log string into logcat chunks
+						char* logBufferEnd = logBuffer + logBytes;   
+						for(char* logBufferPtr = logBuffer;;) {
+							
+							uint32 logBufferPtrAdvance;
+							uint32 logBufferPtrBreakpoint;
+
+							//search for whitespace to split the log
+							uint32 whitespaceBreakpoint = [&]() {
+								
+								for(uint32 i = kLogcatMaxPayloadBytes; i; --i) {	
+									switch(logBufferPtr[i]) {
+										case ' ':
+										case '\t':
+										case '\r':
+										case '\f':
+										case '\n': return i;
+									}
+								}
+								return 0u;
+							}();
+
+							if(whitespaceBreakpoint) {
+								
+								//Jump over whitespace in next loop
+								logBufferPtrAdvance = whitespaceBreakpoint+1;
+								logBufferPtrBreakpoint = whitespaceBreakpoint;
+
+							} else {
+
+								//search for nice charter to break on
+								logBufferPtrBreakpoint = [&]() {
+
+									for(uint32 i = kLogcatMaxPayloadBytes; i; --i) {	
+										switch(logBuffer[i]) {
+											case '.':
+											case '-': return i;
+										}
+									}
+
+									//Didn't find a nice character just split on max size
+									return kLogcatMaxPayloadBytes; 
+								}();
+								logBufferPtrAdvance = logBufferPtrBreakpoint;
+							}
+
+							//replace breakpoint with null
+							char logBufferPtrBreakpointChar = logBufferPtr[logBufferPtrBreakpoint];
+							logBufferPtr[logBufferPtrBreakpoint] = '\0';
+							
+							//write to logcat
+							__android_log_write(logPriority, kLogcatTag, logBufferPtr);
+					
+							//restore breakpoint
+							logBufferPtr[logBufferPtrBreakpoint] = logBufferPtrBreakpointChar;
+						
+							//advance logBufferPtr
+							logBufferPtr+= logBufferPtrAdvance;
+
+							//check to see if we're done
+							uint32 remainingLogBytes = ByteDistance(logBufferEnd, logBufferPtr); 
+							if(remainingLogBytes <= kLogcatMaxPayloadBytes) {
+								
+								//do the last copy
+								__android_log_write(logPriority, kLogcatTag, logBufferPtr);
+								break;
+							}
+						}
+
+						HeapFree(logBuffer, logBytes);
+					}
+
+
 				}
 			}
 
